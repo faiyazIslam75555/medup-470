9fc2950d73357b55e526213cb72a71bff6a2df7a
faiyazIslam75555 <151964572+faiyazIslam75555@users.noreply.github.com>
2025-08-13 01:12:14 +0600

    Delete backend directory

--- backend/models/Specialty.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const specialtySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  symptoms: [{ type: String, required: true }]
-});
-
-module.exports = mongoose.model('Specialty', specialtySchema);
